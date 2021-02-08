17134737b8df5d161bdb83129666a4238c0dfce5
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-08 10:52:46 +0100

    initial setup

---
+++ js07.js
@@ -0,0 +1,7 @@
+// Javascript For Loop
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+var text = "";
+for (i = 0; i < cars.length; i++) {
+    text += cars[i] + "\n";
+}
+console.log(text);
