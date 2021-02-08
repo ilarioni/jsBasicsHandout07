96d85f8de6eb5eb690b991922ab3a9772bb92bbe
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-08 10:53:10 +0100

    types of loop

--- js07.js
+++ js07.js
@@ -5,3 +5,10 @@ for (i = 0; i < cars.length; i++) {
     text += cars[i] + "\n";
 }
 console.log(text);
+
+// different kinds of loops
+// 1 for - loops through a block of code a number of times
+// 2 for/in - loops through the properties of an object
+// 3 for/of - loops through the values of an iterable object
+// 4 while - loops through a block of code while a specified condition is true
+// 5 do/while - also loops through a block of code while a specified condition is true
